811047180bda5b7be5835ffae9dd0e768a02a0bb
Ryan Chesler <ryan@itconnected.tech>
2023-12-25 18:30:37 -0800

    updated 3d_pretraining

--- 3d_pretraining.py
+++ 3d_pretraining.py
@@ -1,43 +1,22 @@
 from torch.utils.data import DataLoader
-from torch.cuda.amp import autocast, GradScaler
-import cv2
+from torch.cuda.amp import GradScaler
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 from tqdm.auto import tqdm
-import glob
 import torch
 import torch.nn as nn
-from torch.optim import Adam, SGD, AdamW
-import torch.nn.functional as F 
+from torch.optim import AdamW
 
 from torch.utils.data import DataLoader, Dataset
 import cv2
-import torch
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
-import pandas as pd
-from externals.utils import set_seed, make_dirs, cfg_init
-from externals.dataloading import read_image_mask, get_train_valid_dataset, get_transforms, CustomDataset
-from externals.models import effnet_v2, resnet18, effnet_v2_half, resnet18_regression, \
-                             effnetv2_regression, effnetv2_m_regression, resnet_long_regression, resnet_short_regression, Unet3D_full3d, resnet18_3d, Unet3D_full3d_deep,\
-                             Unet3D_full3d_shallow, Unet3D_full3d_xxl
-from externals.metrics import AverageMeter, calc_fbeta, fbeta_numpy
+from externals.utils import cfg_init
+from externals.models import Unet3D_full3d_shallow
+from externals.metrics import AverageMeter
 from externals.training_procedures import get_scheduler, scheduler_step
-# from externals.postprocessing import post_process
-from torch.optim.swa_utils import AveragedModel, SWALR
 import wandb
-import timm
-import ast
 import h5py
-import segmentation_models_pytorch as smp
-from monai.networks.nets.unetr import UNETR
-from scipy.ndimage.filters import gaussian_filter
 import time
-# dl = smp.losses.DiceLoss(mode="binary", ignore_index=-1, smooth=0)
-# bce = smp.losses.SoftBCEWithLogitsLoss(ignore_index=-1, smooth_factor=0, reduction="none")
 mse = nn.MSELoss()
-from volumentations import *
 
 def criterion(y_preds, y_true):
     return (
@@ -64,8 +43,8 @@ class CFG:
     # ============== model cfg =============
     model_name = '3d_unet'
     # ============== training cfg =============
-    size = 64
-    tile_size = 64
+    size = 128
+    tile_size = 128
     in_chans = 1
 
     train_batch_size = 128
@@ -102,70 +81,10 @@ class CFG:
 
 
 cfg_init(CFG)
-def get_augmentation():
-    return Compose([
-        # Rotate((-15, 15), (-15, 15), (-15, 15), p=0.5),
-        # RandomResizedCrop(shape=(CFG.size, CFG.size, CFG.size), p=0.9),
-        # ElasticTransform((0, 0.25), interpolation=2, p=0.1),
-        # Resize((CFG.size, CFG.size, CFG.size), interpolation=1, resize_type=0, always_apply=True, p=1.0),
-        # Flip(0, p=0.5),
-        # Flip(1, p=0.5),
-        # Flip(2, p=0.5),
-        # RandomRotate90((0, 1), p=0.1),
-        # RandomRotate90((0, 2), p=0.1),
-        # RandomRotate90((1, 2), p=0.1),
-        # GaussianNoise(var_limit=(0, 5), p=0.2),
-        # RandomGamma(gamma_limit=(80, 120), p=0.2),
-        # GridDropout(unit_size_min=10, unit_size_max=30, holes_number_x=10, holes_number_y=10, holes_number_z=10, p = 0.4)
-    ], p=1.0)
-
-aug = get_augmentation()
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# model = effnet_v2_half(CFG)
-# model = resnet18_regression(CFG)
 model = Unet3D_full3d_shallow(CFG)
-# model = resnet18_3d(CFG)
-
-# model = UNETR(in_channels=1, out_channels=1, img_size=(CFG.size,CFG.size,CFG.size), proj_type='conv', norm_name='instance', )
-# model = timm.create_model('resnet18', pretrained=True, num_classes=2, in_chans = 1)
-from scipy import ndimage
-# class CustomDataset(Dataset):
-#     def __init__(self, volume_path, cfg, labels=None, transform=None, mode="test", size=1000, coords = None):
-#         self.volumes = volume_path
-#         self.cfg = cfg
-#         self.labels = labels
-#         self.transform = transform
-#         self.mode = mode
-#         self.size = size
-#         self.coords = coords
-#         self.file = h5py.File("/data/volume.hdf5", 'r')
-#         self.shape = self.file["scan_volume"].shape
-            
-#     def __len__(self):
-#         return len(self.coords)
-
-#     def __getitem__(self, idx):
-#         invalid_volume = True
-#         while invalid_volume:
-#             coords = [np.random.randint(0, self.shape[0]-self.cfg.size), np.random.randint(0, self.shape[1]-self.cfg.size), np.random.randint(0, self.shape[2]-self.cfg.size)]
-#             volume = self.file["scan_volume"][coords[0]:(coords[0]+(self.cfg.size)),
-#                                     coords[1]:(coords[1]+((self.cfg.size))),
-#                                     coords[2]:(coords[2]+((self.cfg.size)))]/255.
-#             if volume.max() > 0.5:
-#                 invalid_volume = False
-#         volume = volume.astype(np.float16)
-#         image = volume.copy()
-#         for _ in range(4):
-#             size_x = random.randint(CFG.size//4, CFG.size//1.5)
-#             size_y = random.randint(CFG.size//4, CFG.size//1.5) 
-#             size_z = random.randint(CFG.size//4, CFG.size//1.5) 
-#             x = random.randint(0, volume.shape[0] - size_x)
-#             y = random.randint(0, volume.shape[1] - size_y)
-#             z = random.randint(0, volume.shape[2] - size_z)
-#             image[x:x+size_x, y:y+size_y, z:z+size_z] = -1
-#         return image[None], volume[None]
 
 class CustomDataset(Dataset):
     def __init__(self, volume_path, cfg, labels=None, transform=None, mode="test", size=1000, coords=None, cache_size=10000):
@@ -176,14 +95,17 @@ class CustomDataset(Dataset):
         self.mode = mode
         self.size = size
         self.coords = coords
-        self.file = h5py.File("/data/volume_compressed.hdf5", 'r')
-        self.shape = self.file["scan_volume"].shape
+        self.file = h5py.File("/mnt/aged-star/volume.hdf5", 'r')
+        self.shape = self.file["20230205180739"].shape
         self.cache_size = cache_size
         self.cache = {}
         self.cache_indices = []
 
     def __len__(self):
-        return len(self.coords)
+        if self.coords is not None:
+            return len(self.coords)
+        else:
+            return self.size
 
     def preload_cache(self):
         # Clear cache
@@ -202,20 +124,17 @@ class CustomDataset(Dataset):
             coords = [np.random.randint(0, self.shape[0] - self.cfg.size),
                       np.random.randint(0, self.shape[1] - self.cfg.size),
                       np.random.randint(0, self.shape[2] - self.cfg.size)]
-            volume = self.file["scan_volume"][coords[0]: (coords[0] + (self.cfg.size)),
+            volume = self.file["20230205180739"][coords[0]: (coords[0] + (self.cfg.size)),
                                               coords[1]: (coords[1] + ((self.cfg.size))),
                                               coords[2]: (coords[2] + ((self.cfg.size)))] / 255.
             if volume.max() > 0.7 and volume.min() < 0.3:
                 invalid_volume = False
         volume = volume.astype(np.float16)
         image = volume.copy()
-        # image[image.shape[0]//8:-image.shape[0]//8,
-        #       image.shape[1]//8:-image.shape[1]//8,
-        #       image.shape[2]//8:-image.shape[2]//8] = -1
         for _ in range(4):
-            size_x = random.randint(self.cfg.size // 3, self.cfg.size // 2)
-            size_y = random.randint(self.cfg.size // 3, self.cfg.size // 2)
-            size_z = random.randint(self.cfg.size // 3, self.cfg.size // 2)
+            size_x = random.randint(self.cfg.size // 2, self.cfg.size // 1.3)
+            size_y = random.randint(self.cfg.size // 2, self.cfg.size // 1.3)
+            size_z = random.randint(self.cfg.size // 2, self.cfg.size // 1.3)
             x = random.randint(0, volume.shape[0] - size_x)
             y = random.randint(0, volume.shape[1] - size_y)
             z = random.randint(0, volume.shape[2] - size_z)
@@ -224,7 +143,7 @@ class CustomDataset(Dataset):
         return image[None], volume[None]
 
     def __getitem__(self, idx):
-        if (random.random() > 0.15) and (len(self.cache_indices) > 100):
+        if (random.random() > 0.5) and (len(self.cache_indices) > 1000):
             while True:
                 try:
                     random_idx = random.choice(self.cache_indices)
@@ -300,40 +219,32 @@ def valid_fn(valid_loader, model, criterion, device):
     return losses.avg
 
 import random
-import monai
-    
-coords = np.load("/data/depth_narrow_filtered.npy")
-print(coords.shape)
-
-from sklearn.model_selection import train_test_split
-
-train_coords, validation_coords = train_test_split(coords, test_size=.01, shuffle=False)
-training_dataset = CustomDataset(volume_path="/data/volume.hdf5", labels="depth_narrow_train.hdf5", cfg=CFG, transform=None, mode="train", size = 1000000, coords=train_coords)
+training_dataset = CustomDataset(volume_path="/data/volume.hdf5", labels="depth_narrow_train.hdf5", cfg=CFG, transform=None, mode="train", size = 1000000, coords=None)
 sampler = torch.utils.data.RandomSampler(training_dataset, replacement=True, num_samples=100000)
 train_loader = DataLoader(training_dataset, batch_size=CFG.train_batch_size, shuffle=False, num_workers=16, pin_memory=False, drop_last=True, sampler=sampler)
 
-valid_dataset = CustomDataset(volume_path="/data/volume.hdf5", labels="depth_narrow_train.hdf5", cfg=CFG, transform=None, size = 1000, coords=validation_coords)
-
-valid_loader = DataLoader(valid_dataset, batch_size=CFG.train_batch_size, shuffle=False, num_workers=4, pin_memory=False)
 cfg_pairs = {value:CFG.__dict__[value] for value in dir(CFG) if value[1] != "_"}
 model_name = f"{CFG.exp_name}_{CFG.model_name}"
 
-if os.path.exists("/home/ryanc/kaggle/working/outputs/vesuvius_3d/pretrain/vesuvius_3d-models/pretrain_3d_unet.pth"):
-    print("/home/ryanc/kaggle/working/outputs/vesuvius_3d/pretrain/vesuvius_3d-models/pretrain_3d_unet.pth")
-    model.load_state_dict(torch.load("/home/ryanc/kaggle/working/outputs/vesuvius_3d/pretrain/vesuvius_3d-models/pretrain_3d_unet.pth"))
+# if os.path.exists("/home/ryanc/kaggle/working/outputs/vesuvius_3d/pretrain/vesuvius_3d-models/pretrain_3d_unet.pth"):
+#     print("/home/ryanc/kaggle/working/outputs/vesuvius_3d/pretrain/vesuvius_3d-models/pretrain_3d_unet.pth")
+#     model.load_state_dict(torch.load("/home/ryanc/kaggle/working/outputs/vesuvius_3d/pretrain/vesuvius_3d-models/pretrain_3d_unet.pth"))
 
 model = torch.nn.DataParallel(model)
 model.to(device)
 
 optimizer = AdamW(model.parameters(), lr=CFG.lr, weight_decay=CFG.weight_decay)
 scheduler = get_scheduler(CFG, optimizer)
+
+wandb.init(
+    project="LSM"
+)
+
 for epoch in range(CFG.epochs):
     # train
     avg_loss = train_fn(train_loader, model, criterion, optimizer, device)
     torch.save(model.module.state_dict(),
             CFG.model_dir + f"{model_name}.pth")
-    avg_val_loss = valid_fn(
-        valid_loader, model, criterion, device)
-    print({"avg_train_loss":avg_loss, "avg_val_loss":avg_val_loss})
-    
-    scheduler_step(scheduler, avg_val_loss, epoch)
+    wandb.log({"avg_train_loss":avg_loss})
+    print({"avg_train_loss":avg_loss})
+    scheduler_step(scheduler, None, epoch)
