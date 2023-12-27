2ca6566d2a1ceeaec4b342983ab09002d846d80b
Ryan Chesler <ryan@itconnected.tech>
2023-12-27 07:53:50 -0800

    Update models.py

--- externals/models.py
+++ externals/models.py
@@ -1,6 +1,15 @@
 from pytorch3dunet.unet3d.model import get_model
+from monai.networks.nets.unetr import UNETR
 from torch import nn
-    
+
+class Unetr(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.model = UNETR(in_channels=1, out_channels=1, img_size=(CFG.size,CFG.size,CFG.size), proj_type='conv', norm_name='instance', )
+    def forward(self, volume):
+        output = self.model(volume)
+        return output
+
 class Unet3D_full3d_shallow(nn.Module):
     def __init__(self, cfg):
         super().__init__()
